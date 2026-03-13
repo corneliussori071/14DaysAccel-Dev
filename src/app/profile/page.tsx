08a6f934a5bffa0a3db8007db3338f97473a0dca
Buntypedia <corneliussori071@gmail.com>
2026-03-13 13:40:58 +0000

    feat: add /profile route for user account page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import ProfileLayout from "@/components/profile/ProfileLayout";
+
+export const metadata = {
+  title: "My Account - 14DaysAccel Dev",
+};
+
+export default function ProfilePage() {
+  return <ProfileLayout />;
+}
